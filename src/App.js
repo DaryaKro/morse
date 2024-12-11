32b9316b041b36d107b892d75118a26c037662ab
Darya Kropotova <kropotovada@hotmail.com>
2024-12-11 16:32:41 +0300

    Change p on textarea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { textToMorse, textToBinary } from './translatorFunctions';
+import { textToMorse, textToBinary, morseToText, binaryToText } from './translatorFunctions';
 
 function App() {
   const [input, setInput] = useState('');
@@ -14,23 +14,48 @@ function App() {
     setBinaryOutput(textToBinary(userInput));
   };
 
+  const handleMorseChange = (e) => {
+    const morseInput = e.target.value;
+    setMorseOutput(morseInput);
+    setInput(morseToText(morseInput));
+    setBinaryOutput(textToBinary(morseToText(morseInput)));
+  };
+
+  const handleBinaryChange = (e) => {
+    const binaryInput = e.target.value;
+    setBinaryOutput(binaryInput);
+    setInput(binaryToText(binaryInput));
+    setMorseOutput(textToMorse(binaryToText(binaryInput)));
+  };
+
   return (
     <div className="App">
       <div classname='mainBlock'>
         <h1>Перевод фразы в код морзе или двоичный код</h1>
-        <textarea
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Напишите любую фразу"
-        />
         <div className='blocks'>
           <div className='block'>
+            <h2>Текст</h2>
+            <textarea
+              value={input}
+              onChange={handleInputChange}
+              placeholder="Напишите любую фразу"
+            />
+          </div>
+          <div className='block'>
             <h2>Код морзе</h2>
-            <span>{morseOutput ? morseOutput : <span className='empty'>Напишите фразу для перевода</span>}</span>
+            <textarea
+              value={morseOutput}
+              onChange={handleMorseChange}
+              placeholder="Напишите код морзе"
+            />
           </div>
           <div className='block'>
             <h2>Двоичный код</h2>
-            <span>{binaryOutput ? binaryOutput : <span className='empty'>Напишите фразу для перевода</span>}</span>
+            <textarea
+              value={binaryOutput}
+              onChange={handleBinaryChange}
+              placeholder="Напишите двоичный код"
+            />
           </div>
         </div>
       </div>
